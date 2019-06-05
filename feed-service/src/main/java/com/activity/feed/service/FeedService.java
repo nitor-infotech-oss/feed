d9c8b9896319f725ff8b1173457a684f163b1e54
pramoddhone <pramoddhone@yahoo.com>
2019-06-05 18:10:05 +0530

    Commit Feedservice

--- feed-service/src/main/java/com/activity/feed/service/FeedService.java
+++ feed-service/src/main/java/com/activity/feed/service/FeedService.java
@@ -1,5 +1,32 @@
 package com.activity.feed.service;
 
-public class FeedService {
+import java.util.List;
+import java.util.Optional;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.activity.feed.model.Feed;
+import com.activity.feed.model.dao.FeedRepository;
+
+@Service
+public class FeedService {
+	
+	@Autowired
+	private FeedRepository feedRepository;
+	
+	@Transactional
+	public Feed getFeed(Long id) {
+		Optional<Feed> optionalFeed = feedRepository.findById(id);
+		if(optionalFeed.isPresent()) {
+			return optionalFeed.get();
+		}
+		return null;
+	}
+	
+	public List<Feed> getFeeds(String userId) {
+		
+		return null;
+	}
 }
